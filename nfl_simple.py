2956d192dd6915b3b5a8e6978ea762e75f963948
adriftfordays <136281816+adriftfordays@users.noreply.github.com>
2025-11-26 20:55:52 -0500

    Fix NameError - retype first_game variable

--- nfl_simple.py
+++ nfl_simple.py
@@ -4,7 +4,10 @@ url = "https://www.thesportsdb.com/api/v1/json/3/eventsnextleague.php?id=4291"
 response = requests.get(url)
 data = response.json()
 
-first_game= data['events'][0]
+print("Got data:", type(data))
+print("Number of events:", len(data['events']))
+
+first_game = data['events'][0]
 away = first_game['strAwayTeam']
 home = first_game['strHomeTeam']
 
