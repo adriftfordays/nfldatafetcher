0948513863250b00607af7a59e21cda01cc4b5e8
adriftfordays <matthewrbland@gmail.com>
2025-11-26 22:44:45 -0500

    Updated to For Loops

--- nfl_simple.py
+++ nfl_simple.py
@@ -4,8 +4,7 @@ url = "https://www.thesportsdb.com/api/v1/json/123/eventsnextleague.php?id=4391"
 response = requests.get(url)
 data = response.json()
 
-first_game = data['events'][0]
-away = first_game['strAwayTeam']
-home = first_game['strHomeTeam']
-
-print(f"Next Game: {away} @ {home}")
+for game in data ['events'][:5]:
+    away = game['strAwayTeam']
+    home = game['strHomeTeam']
+    print(f"Next Game: {away} @ {home}")
