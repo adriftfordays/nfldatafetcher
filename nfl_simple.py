d0b305ddf1087cf28f44c92de044185d7572ebd6
adriftfordays <136281816+adriftfordays@users.noreply.github.com>
2025-11-26 21:31:01 -0500

    Fixed league ID

--- nfl_simple.py
+++ nfl_simple.py
@@ -1,11 +1,8 @@
 import requests
 
-url = "https://www.thesportsdb.com/api/v1/json/3/eventsnextleague.php?id=4291"
+url = "https://www.thesportsdb.com/api/v1/json/123/eventsnextleague.php?id=4391"
 response = requests.get(url)
 data = response.json()
-
-print("Got data:", type(data))
-print("Number of events:", len(data['events']))
 
 first_game = data['events'][0]
 away = first_game['strAwayTeam']
